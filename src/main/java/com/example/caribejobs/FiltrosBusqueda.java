a8515f4af9d56c14dbc6634b201f4c959ac3194b
Adriel0312 <55627979+Adriel0312@users.noreply.github.com>
2020-07-30 22:52:44 -0600

    Update FiltrosBusqueda.java

--- src/main/java/com/example/caribejobs/FiltrosBusqueda.java
+++ src/main/java/com/example/caribejobs/FiltrosBusqueda.java
@@ -35,6 +35,9 @@ public class FiltrosBusqueda extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
+        
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtros_busqueda);
 
@@ -63,6 +66,70 @@ public class FiltrosBusqueda extends AppCompatActivity {
         JSONArray res = consulta.getProvincias();
         listaProvincias = new ArrayList<String>();
         listaProvincias.add("Seleccione una provincia");
+        
+        
+        
+        Persona persona = new Persona();
+        API consulta = new API();
+        JSONArray res = consulta.getProfesion(correoSesion);
+
+        for(int i=0; i<res.length();i++){
+            try {
+                JSONObject json = res.getJSONObject(i);
+                mLista.add(json.getString("profesion"));
+                mAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,mLista);
+                listProfesiones.setAdapter(mAdapter);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.d("Error",e.toString());
+            }
+        }
+
+ super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+        nuevaProfesion = (Button) findViewById(R.id.nuevaProfesion);
+        correoSesion = getIntent().getStringExtra("parametro");
+        listProfesiones = (ListView)findViewById(R.id.listProfesiones);
+        //correoSesion = getIntent().getStringExtra("parametro");
+        Log.d("Parametro",correoSesion);
+
+        cargarProfesiones();
+
+        nuevaProfesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Flag","Flag4");
+                Intent intent = new Intent(Registro.this, Profesion1.class);
+                intent.putExtra("parametro", correoSesion);
+                startActivity(intent);
+            }
+        });
+
+        mBottonNavigation = (BottomNavigationView) findViewById(R.id.botonNavegacion);
+
+        mBottonNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.menu_buscar){
+                    Intent intent = new Intent(Registro.this, FiltrosBusqueda2.class);
+                    intent.putExtra("parametro", correoSesion.toString());
+                    startActivity(intent);
+                }
+                if(item.getItemId() == R.id.menu_perfil){
+                    Intent intent = new Intent(Registro.this, Perfil.class);
+                    intent.putExtra("parametro", correoSesion.toString());
+                    startActivity(intent);
+                }
+                if(item.getItemId() == R.id.menu_registro){
+                    Intent intent = new Intent(Registro.this, Registro.class);
+                    intent.putExtra("parametro", correoSesion.toString());
+                    startActivity(intent);
+                }
+                return true;
+            }
+        });
+
         for(int i=0; i<res.length();i++){
             try {
                 JSONObject json = res.getJSONObject(i);
